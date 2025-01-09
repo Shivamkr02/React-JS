3f159d496bdc9bf56dfa9a2f4747e3cdf4d5ff69
shivamkr02 <shivamvts05@gmail.com>
2025-01-09 15:47:44 +0530

    Default export

---
+++ Module Examples/Example 9/main.js
@@ -0,0 +1,3 @@
+import total,{subtract} from "./math.js";
+total(5,20);
+subtract(50,20);
---
+++ Module Examples/Example 9/math.js
@@ -0,0 +1,11 @@
+export default (a,b)=>{
+    let c = a+b;
+
+    console.log(`Sum is ${c}`);
+    
+}
+
+export function subtract(a,b){
+    let c = a - b;
+    console.log(`Diff is ${c}`);
+}
