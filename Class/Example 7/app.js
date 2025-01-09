96d605eab6d9b447fb0d7fbf5fd9acd237dca30a
shivamkr02 <shivamvts05@gmail.com>
2025-01-09 16:49:39 +0530

    this in constructor

---
+++ Class/Example 7/app.js
@@ -0,0 +1,10 @@
+class Emp{
+    constructor() { 
+        this.id = 101;
+        this.name = "Shristi";
+        this.sal = 90000.0;
+    }
+}
+
+let e1 = new Emp();
+console.log(`id: ${e1.id}, name: ${e1.name}, sal: ${e1.sal}`);
