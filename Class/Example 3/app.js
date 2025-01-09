eb962fa4e5ef2b76a39430bb257324804cddba34
shivamkr02 <shivamvts05@gmail.com>
2025-01-09 16:29:05 +0530

    changes

--- Class/Example 3/app.js
+++ Class/Example 3/app.js
@@ -9,4 +9,4 @@ let e1 = new Emp();
 console.log(`id: ${e1.id}, name: ${e1.name}, sal: ${e1.sal}`);
 
 let e2 = new Emp();
-console.log(`id: ${e1.id}, name: ${e1.name}, sal: ${e1.sal}`);
+console.log(`id: ${e2.id}, name: ${e2.name}, sal: ${e2.sal}`);
