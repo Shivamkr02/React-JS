fd60ff84a41ad1d84fa0da8ecf38664308ae7e30
shivamkr02 <shivamvts05@gmail.com>
2025-01-09 16:30:07 +0530

    Class

---
+++ Class/Example 4/app.js
@@ -0,0 +1,18 @@
+class Emp{
+    id;
+    name;
+    sal;
+}
+
+let e1 = new Emp();
+e1.id = 101;
+e1.name = "Shristi";
+e1.sal = 90000.0;
+
+console.log(`id: ${e1.id}, name: ${e1.name}, sal: ${e1.sal}`);
+
+let e2 = new Emp();
+e2.id = 102;
+e2.name = "Shivam";
+e2.sal = 120000.0;
+console.log(`id: ${e2.id}, name: ${e2.name}, sal: ${e2.sal}`);
