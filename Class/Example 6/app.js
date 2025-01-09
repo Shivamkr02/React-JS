720a29f443c40d3cc0ee923c5cae2c0cc9a64291
shivamkr02 <shivamvts05@gmail.com>
2025-01-09 16:42:52 +0530

    Class

---
+++ Class/Example 6/app.js
@@ -0,0 +1,13 @@
+class Emp{
+    constructor() { 
+        console.log(`Object created and constructor called...`);
+    }
+}
+
+let e1 = new Emp();
+
+let e2 = new Emp();
+
+let e3 = new Emp();
+
+console.log(typeof Emp);
