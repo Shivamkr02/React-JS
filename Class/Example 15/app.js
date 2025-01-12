2c0155a58798dfe7aeada3dc0ae2d8dd2a9a0ee8
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2025-01-12 16:23:11 +0530

    Add files via upload
    Class

---
+++ Class/Example 15/app.js
@@ -0,0 +1,13 @@
+class Vehicle {
+    accelarate() {
+        document.write(`Vehicle is accelerating...<br>`);
+    }
+}
+class Car extends Vehicle {
+    reverse() {
+        document.write(`Car is reversing...<br>`);
+    }
+}
+let audi = new Car();
+audi.accelarate();
+audi.reverse();
