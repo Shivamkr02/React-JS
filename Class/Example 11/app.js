b2994adc56ee5eeff54adf3756ebff419b24650d
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2025-01-12 16:19:51 +0530

    Add files via upload
    Class

---
+++ Class/Example 11/app.js
@@ -0,0 +1,14 @@
+// This will Run but it will give undefined
+class Emp{
+    constructor(id,name,sal) { 
+        this.id = id;
+        this.name = name;
+        this.sal = sal;
+    }
+}
+
+let e1 = new Emp(101,"Shristi",80000.0);
+console.log(`id: ${e1.id}, name: ${e1.name}, sal: ${e1.sal}`);
+
+let e2 = new Emp(); // This will give undefined output
+console.log(`id: ${e2.id}, name: ${e2.name}, sal: ${e2.sal} `)
