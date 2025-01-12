b481dfaac290dd543d5fd2d96ff453578d428e0a
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2025-01-12 16:13:29 +0530

    Add files via upload
    class

---
+++ Class/Example 8/app.js
@@ -0,0 +1,11 @@
+// This will not run
+class Emp{
+    constructor() { 
+        id = 101;
+        names = "Shristi";
+        sal = 90000.0;
+    }
+}
+
+let e1 = new Emp();
+console.log(`id: ${e1.id}, name: ${e1.name}, sal: ${e1.sal}`);
