df2108b94d212b0190af7f7b62bd752bff225f91
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2025-01-13 23:27:55 +0530

    Add files via upload
    Class

---
+++ Class/Example 18/app.js
@@ -0,0 +1,20 @@
+class Vehicle {
+    constructor(){
+        document.write(`Vehicle Created...<br>`)
+    }
+    accelarate() {
+        document.write(`Vehicle is accelerating...<br>`);
+    }
+}
+class Car extends Vehicle {
+    constructor(){
+        document.write(`Vehicle Created...<br>`);
+        super(); // This line is Compulsory
+    }
+    reverse() {
+        document.write(`Car is reversing...<br>`);
+    }
+}
+let audi = new Car();
+audi.accelarate();
+audi.reverse();
