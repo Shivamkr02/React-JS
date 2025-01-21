24130b151fe7951f2d5ace2e52354daa11fa09e7
shivamkr02 <shivamvts05@gmail.com>
2025-01-21 15:22:14 +0530

    commit

--- Class/Example 26/app.js
+++ Class/Example 26/app.js
@@ -5,6 +5,9 @@ class Emp {
         this.sal = sal;
         document.write("Emp object created...<br>");
     }
+    raiseIncome(){
+        this.sal = this.sal + this.sal * 0.1;
+    }
     toString(){
         return `id: ${this.id}, name: ${this.name}, sal: ${this.sal}`;
     }
@@ -15,9 +18,16 @@ class Manager extends Emp{
         this.bonus = bonus;
         document.write("Manager object created...<br>");
     }
-    toString(){
+    raiseIncome(){   // This is called method overriding
+        super.raiseIncome();
+        this.bonus = this.bonus+this.bonus*0.1;
+    }
+    toString(){     // This is called method overriding
         return `${super.toString()}, Bonus: ${this.bonus}`;
     }
 }
 let boss = new Manager(101,"Shristi",80000.0,5000.0);
 document.write(`${boss}<br>`);
+document.write("<br>After Increment <br>");
+boss.raiseIncome();
+document.write(`${boss}`);
