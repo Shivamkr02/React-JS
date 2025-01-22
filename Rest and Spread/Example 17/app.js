c476b6f9ce723e54525be2a8a7769dbda423d305
shivamkr02 <shivamvts05@gmail.com>
2025-01-23 00:08:05 +0530

    commit

---
+++ Rest and Spread/Example 17/app.js
@@ -0,0 +1,12 @@
+let student1 = {
+    roll: 12,
+    name: "Shivam",
+    marks: {phy: 50, chem: 60, maths: 70},
+};
+let student2 = {...student1};
+document.write(student1 === student2); // false
+document.write("<br>");
+student2.marks.phy = 100;
+document.write(student2.marks.phy); // 100
+document.write("<br>");
+document.write(student1.marks.phy); // 100
