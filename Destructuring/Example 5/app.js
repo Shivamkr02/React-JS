a4d525b9f8a3d4519199182470622ac6ba8f6691
shivamkr02 <shivamvts05@gmail.com>
2025-01-23 00:05:01 +0530

    commit

---
+++ Destructuring/Example 5/app.js
@@ -0,0 +1,3 @@
+let person = [21,"Shristi",45000,["B.tech","M.tech","Phd"]];
+let [age,name,salary,degree] = person; // Using default value with destructuring
+document.write(`${age},${name},${salary},${degree}`);
