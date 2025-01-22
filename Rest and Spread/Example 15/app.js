4f5374360035558dc1098966aa57bf4484cb69e0
shivamkr02 <shivamvts05@gmail.com>
2025-01-23 00:07:36 +0530

    commit

---
+++ Rest and Spread/Example 15/app.js
@@ -0,0 +1,5 @@
+let person = {age: 23, name: "Shristi"};
+let address = {city: "Bhopal", Locality: "Indrapuri"};  // Deep Copy
+document.write(person1 === person2);
+document.write(`${person1.age},${person1.name}<br>`);
+document.write(`${person2.age},${person2.name}<br>`);
