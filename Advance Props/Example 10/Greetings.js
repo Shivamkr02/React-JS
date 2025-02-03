10267edb280584dd6e3f1db665ccb6d5d8ffa6c9
shivamkr02 <shivamvts05@gmail.com>
2025-02-03 22:25:06 +0530

    commit

---
+++ Advance Props/Example 10/Greetings.js
@@ -0,0 +1,9 @@
+class Greetings extends React.Component{
+    render(){
+        return (
+            <h1>
+                Happy New Year TO {this.props.who} From {this.props.from}
+            </h1>
+        );
+    }
+} 
