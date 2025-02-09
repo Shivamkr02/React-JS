df60071ed57e3ad072b62d93f9f52b3daacd49f1
shivamkr02 <shivamvts05@gmail.com>
2025-02-09 21:19:17 +0530

    commit

--- React Using Vite/myapp2/src/Fruits.jsx
+++ React Using Vite/myapp2/src/Fruits.jsx
@@ -1,11 +1,18 @@
-import React, {Component} from 'react'
-import foods from  from './foods'
+import React, {Component} from "react";
+import foods from "./foods";
+import {choice,remove} from "./helper";
 export default class Fruits extends Component {
     render() {
+        let randomFruit=choice(foods);
+        let remainingFruits=remove(foods,randomFruit);
         return (
             <div>
                 <p>Total fruits: {foods}</p>
+                <p>They are : {foods.join()}</p>
+                <p>Chosen fruit:{choice(foods)}</p>
+                <p>Remaining Fruits: {remainingFruits.length}</p>
+                <p>They are: {remainingFruits.join()}</p>
             </div>
-        )
+        );
     }
 }
