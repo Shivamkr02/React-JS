0695d9619d039e4b6633e9e0892c1d4115da1946
shivamkr02 <shivamvts05@gmail.com>
2025-02-17 06:56:30 +0530

    commit

---
+++ React Using Vite/eventdemo1/src/MyButton.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+
+export default class MyButton extends Component {
+    handleClick = () => {
+        alert(new Date());
+    };
+    render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    )
+  }
+}
